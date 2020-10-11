349dadf61433a20c5d760bef11e5f45311ff80d1
aditya Kharade <adityakharade51@gmail.com>
2020-10-11 16:44:14 +0530

    Added : added functionality to find max string

--- src/testmaximum/MaximumTest.java
+++ src/testmaximum/MaximumTest.java
@@ -10,48 +10,72 @@ class MaximumTest {
 	public void givenFirstValueShouldReturnTrue() {
 		TestMax test = new TestMax();
 		Integer actual = test.findMaximum(9, 1, 5);
-		Integer expect = 9;
-		assertEquals(expect, actual);
+		Integer expected = 9;
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void givenSecondValueShouldReturnTrue() {
 		TestMax test = new TestMax();
 		Integer actual = test.findMaximum(8, 11, 5);
-		Integer expect = 11;
-		assertEquals(expect, actual);
+		Integer expected = 11;
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void givenThirdValueShouldReturnTrue() {
 		TestMax test = new TestMax();
 		Integer actual = test.findMaximum(9, 1, 15);
-		Integer expect = 15;
-		assertEquals(expect, actual);
+		Integer expected = 15;
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void givenFirstFloatValueShouldReturnTrue() {
 		TestMax test = new TestMax();
 		Float actual = test.findMaximum(9f, 1f, 5f);
-		Float expect = 9f;
-		assertEquals(expect, actual);
+		Float expected = 9f;
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void givenSecondFloatValueShouldReturnTrue() {
 		TestMax test = new TestMax();
 		Float actual = test.findMaximum(2f, 8f, 6f);
-		Float expect = 8f;
-		assertEquals(expect, actual);
+		Float expected = 8f;
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void givenThirdFloatValueShouldReturnTrue() {
 		TestMax test = new TestMax();
 		Float actual = test.findMaximum(9f, 1f, 15f);
-		Float expect = 15f;
-		assertEquals(expect, actual);
+		Float expected = 15f;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void givenFirstStringValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		String actual = test.findMaximum("Peach", "Apple", "Banana");
+		String expected = "Peach";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void givenSecondStringValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		String actual = test.findMaximum("Apple", "Peach", "Banana");
+		String expected = "Peach";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void givenThirdStringValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		String actual = test.findMaximum("Banana", "Apple", "Peach");
+		String expected = "Peach";
+		assertEquals(expected, actual);
 	}
 
 }
--- src/testmaximum/TestMax.java
+++ src/testmaximum/TestMax.java
@@ -41,4 +41,24 @@ public class TestMax {
 		return max;
 	}
 
+	/**
+	 * Usecase3
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	public String findMaximum(String a, String b, String c) {
+		String max;
+		if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
+			max = a;
+		} else if (b.compareTo(c) >= 0 && b.compareTo(a) >= 0) {
+			max = b;
+		} else {
+			max = c;
+		}
+		return max;
+	}
+
 }
