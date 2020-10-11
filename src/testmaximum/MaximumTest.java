e0202c3024ce1224f80e1b83f6a2a300868abc68
aditya Kharade <adityakharade51@gmail.com>
2020-10-11 16:26:47 +0530

    Added : added functionality to find max integer

--- src/testmaximum/MaximumTest.java
+++ src/testmaximum/MaximumTest.java
@@ -6,7 +6,28 @@ import org.junit.jupiter.api.Test;
 
 class MaximumTest {
 
-	
-	
+	@Test
+	public void givenFirstValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		Integer actual = test.findMaximum(9, 1, 5);
+		Integer expect = 9;
+		assertEquals(expect, actual);
+	}
+
+	@Test
+	public void givenSecondValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		Integer actual = test.findMaximum(8, 11, 5);
+		Integer expect = 11;
+		assertEquals(expect, actual);
+	}
+
+	@Test
+	public void givenThirdValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		Integer actual = test.findMaximum(9, 1, 15);
+		Integer expect = 15;
+		assertEquals(expect, actual);
+	}
 
 }
--- src/testmaximum/TestMax.java
+++ src/testmaximum/TestMax.java
@@ -1,5 +1,23 @@
 package testmaximum;
 
 public class TestMax {
-	
+	/**
+	 * Usecase1
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	public Integer findMaximum(Integer a, Integer b, Integer c) {
+		Integer max = 0;
+		if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
+			max = a;
+		} else if (b.compareTo(c) >= 0 && b.compareTo(a) >= 0) {
+			max = b;
+		} else {
+			max = c;
+		}
+		return max;
+	}
+
 }
