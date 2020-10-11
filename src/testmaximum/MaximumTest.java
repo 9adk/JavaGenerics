14d2c2277b25a6242537d81c596c2105267ddebf
aditya Kharade <adityakharade51@gmail.com>
2020-10-11 16:36:12 +0530

    Added : added functionality to find max float

--- src/testmaximum/MaximumTest.java
+++ src/testmaximum/MaximumTest.java
@@ -30,4 +30,28 @@ class MaximumTest {
 		assertEquals(expect, actual);
 	}
 
+	@Test
+	public void givenFirstFloatValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		Float actual = test.findMaximum(9f, 1f, 5f);
+		Float expect = 9f;
+		assertEquals(expect, actual);
+	}
+
+	@Test
+	public void givenSecondFloatValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		Float actual = test.findMaximum(2f, 8f, 6f);
+		Float expect = 8f;
+		assertEquals(expect, actual);
+	}
+
+	@Test
+	public void givenThirdFloatValueShouldReturnTrue() {
+		TestMax test = new TestMax();
+		Float actual = test.findMaximum(9f, 1f, 15f);
+		Float expect = 15f;
+		assertEquals(expect, actual);
+	}
+
 }
--- src/testmaximum/TestMax.java
+++ src/testmaximum/TestMax.java
@@ -3,6 +3,7 @@ package testmaximum;
 public class TestMax {
 	/**
 	 * Usecase1
+	 * 
 	 * @param a
 	 * @param b
 	 * @param c
@@ -10,6 +11,26 @@ public class TestMax {
 	 */
 	public Integer findMaximum(Integer a, Integer b, Integer c) {
 		Integer max = 0;
+		if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
+			max = a;
+		} else if (b.compareTo(c) >= 0 && b.compareTo(a) >= 0) {
+			max = b;
+		} else {
+			max = c;
+		}
+		return max;
+	}
+
+	/**
+	 * Usecase2
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	public Float findMaximum(Float a, Float b, Float c) {
+		Float max;
 		if (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) {
 			max = a;
 		} else if (b.compareTo(c) >= 0 && b.compareTo(a) >= 0) {
